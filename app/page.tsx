0b279fb1d554016fe70a358326df36e63d3c26f7
Emre Turan <itsemreturan@gmail.com>
2023-12-07 00:17:11 +0300

    Add @vercel/postgres dependency and update home page with dynamic quizzes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 import Link from "next/link";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!);
+
+async function Quizzes() {
+  const quizzes = await sql`
+SELECT * FROM quizzes
+`;
+
+  return (
+    <ul>
+      {quizzes.map((quiz) => {
+        return (
+          <li key={quiz.id}>
+            <Link href={`./quiz/${quiz.id}`}>{quiz.name}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 export default function Home() {
   return (
     <main>
       <h1> Quiz App</h1>
-      <ul>
-        <li>
-          <Link href="/quiz/1"> Quiz 1</Link>
-        </li>
-        <li>
-          <Link href="/quiz/2"> Quiz 2</Link>
-        </li>
-        <li>
-          <Link href="/quiz/3"> Quiz 3</Link>
-        </li>
-      </ul>
+      <Quizzes />
     </main>
   );
 }
