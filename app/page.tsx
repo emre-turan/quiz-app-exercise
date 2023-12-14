883bbad8bb680d030d92da4b3318fff535e7121c
Emre Turan <itsemreturan@gmail.com>
2023-12-14 21:38:08 +0300

    Delete unused files and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-import Link from "next/link";
-import postgres from "postgres";
-
-const sql = postgres(process.env.POSTGRES_URL!);
-
-async function Quizzes() {
-  const quizzes = await sql`
-SELECT * FROM quizzes
-`;
-
-  return (
-    <ul>
-      {quizzes.map((quiz) => {
-        return (
-          <li key={quiz.id}>
-            <Link href={`./quiz/${quiz.id}`}>{quiz.name}</Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
 export default function Home() {
   return (
     <main>
       <h1> Quiz App</h1>
-      <Quizzes />
     </main>
   );
 }
--- app/quiz/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function QuizPage({ params }: { params: { id: string } }) {
-  return (
-    <section>
-      <h1>Quiz {params.id}</h1>
-    </section>
-  );
-}
